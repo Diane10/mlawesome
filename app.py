0b637cb18e07c5d208331aae612a7d452fe94a19
Diane10 <46956585+Diane10@users.noreply.github.com>
2020-12-07 22:16:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,6 @@ html_temp = """
 """
 st.markdown(html_temp,unsafe_allow_html=True)
 st.set_option('deprecation.showfileUploaderEncoding', False)
-# st.image("x.png", caption="sample image", width=400)
 st.subheader("Dataset")
 datasetchoice = st.radio("Do you what to use your own dataset?", ("Yes", "No"))
 if datasetchoice=='No':
